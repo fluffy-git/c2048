e174ddc50382923538fe02312594841e0cf9bbbd
fluffy <69675847+fluffy-git@users.noreply.github.com>
2023-09-28 08:56:49 +0200

    print_field update

--- main.c
+++ main.c
@@ -2,18 +2,69 @@
 #include <stdlib.h>
 #include "conioex.h"
 
+void center();
+
 void print_row(int m[4][4], int z){ //field & zeile
 
     for(int i = 0; i<4; i++){
         printf("%c ", 186);
         textcolor(LIGHTRED);
 
-        if(m[z][i]== 0)
-        {
-            printf("  ");
-        }
-        else{
-            printf("%d ", m[z][i]);
+
+        switch(m[z][i]){
+
+            case 0:
+                printf("      ");
+                break;
+
+            case 2:
+                printf("  2   ");
+                break;
+
+            case 4:
+                printf("  4   ");
+                break;
+
+            case 8:
+                printf("  8   ");
+                break;
+
+            case 16:
+                printf(" 16   ");
+                break;
+
+            case 32:
+                printf(" 32   ");
+                break;
+
+            case 64:
+                printf(" 64   ");
+                break;
+
+            case 128:
+                printf(" 128  ");
+                break;
+
+            case 256:
+                printf(" 256  ");
+                break;
+
+            case 512:
+                printf(" 512  ");
+                break;
+
+            case 1024:
+                printf("1024  ");
+                break;
+
+            case 2048:
+                printf("2048  ");
+                break;
+
+            default:
+                printf("ERROR!");
+                break;
+
         }
 
         textcolor(LIGHTGRAY);
@@ -26,26 +77,25 @@ void print_field(int m[4][4]){ // zeile
 
     textcolor(LIGHTGRAY);
 
-    printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", 201, 205, 205, 205, 203, 205, 205, 205, 203, 205, 205, 205, 203, 205, 205, 205, 187);
+    printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", 201, 205, 205, 205, 205, 205, 205, 205, 203, 205, 205, 205, 205, 205, 205, 205, 203, 205, 205, 205, 205, 205, 205, 205, 203, 205, 205, 205, 205, 205, 205, 205, 187);
     print_row(m,0);
-    printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", 204, 205, 205, 205, 206, 205, 205, 205, 206, 205, 205, 205, 206, 205, 205, 205, 185);
+    printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", 204, 205, 205, 205, 205, 205, 205, 205, 206, 205, 205, 205, 205, 205, 205, 205, 206, 205, 205, 205, 205, 205, 205, 205, 206, 205, 205, 205, 205, 205, 205, 205, 185);
     print_row(m,1);
-    printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", 204, 205, 205, 205, 206, 205, 205, 205, 206, 205, 205, 205, 206, 205, 205, 205, 185);
+    printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", 204, 205, 205, 205, 205, 205, 205, 205, 206, 205, 205, 205, 205, 205, 205, 205, 206, 205, 205, 205, 205, 205, 205, 205, 206, 205, 205, 205, 205, 205, 205, 205, 185);
     print_row(m,2);
-    printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", 204, 205, 205, 205, 206, 205, 205, 205, 206, 205, 205, 205, 206, 205, 205, 205, 185);
+    printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", 204, 205, 205, 205, 205, 205, 205, 205, 206, 205, 205, 205, 205, 205, 205, 205, 206, 205, 205, 205, 205, 205, 205, 205, 206, 205, 205, 205, 205, 205, 205, 205, 185);
     print_row(m,3);
-    printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", 200, 205, 205, 205, 202, 205, 205, 205, 202, 205, 205, 205, 202, 205, 205, 205, 188);
-
+    printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", 200, 205, 205, 205, 205, 205, 205, 205, 202, 205, 205, 205, 205, 205, 205, 205, 202, 205, 205, 205, 205, 205, 205, 205, 202, 205, 205, 205, 205, 205, 205, 205, 188);
     textcolor(LIGHTGRAY);
 
 }
 
 int main()
 {
-    int field[4][4] =  {{0,2,0,0},
-                        {0,0,0,0},
-                        {0,0,8,0},
-                        {16,0,0,4}};
+    int field[4][4] =  {{2048,2,0,2048},
+                        {512,0,128,0},
+                        {16,0,8,0},
+                        {8,0,0,4}};
 
     print_field(field);
 
